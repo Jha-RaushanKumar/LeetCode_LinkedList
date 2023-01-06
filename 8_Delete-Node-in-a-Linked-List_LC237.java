aff3dd69b61dde6400e074eda2847a9d1f842afb
Raushan Kumar Jha <jha.rau@northeastern.edu>
2023-01-06 17:17:22 -0500

    Create 8_Delete-Node-in-a-Linked-List_LC237.java

---
+++ 8_Delete-Node-in-a-Linked-List_LC237.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+
+        //Approach1 -- TC:O(1) SC:O(1)
+        node.val = node.next.val;
+        node.next = node.next.next;
+        
+    }
+}
