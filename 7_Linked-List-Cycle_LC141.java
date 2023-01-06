5262d571d74956f8949f9e8778bed12bd989687b
Raushan Kumar Jha <jha.rau@northeastern.edu>
2023-01-06 17:13:59 -0500

    Create 7_Linked-List-Cycle_LC141.java

---
+++ 7_Linked-List-Cycle_LC141.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        //Approach1 -- TC:O(N) SC:O(1)
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
